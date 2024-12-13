66bcc833cfcb5bf00122407857c36794a4942cb4
azrilfahmiardi <anakijummira@gmail.com>
2024-12-13 20:56:17 +0700

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,14 +10,19 @@
 <body>
     <div class="bg-login-gradient font-poppins px-10">
         <div class="min-h-screen w-full grid lg:grid-cols-2 md:grid-cols-1 py-6 px-4">
-            <div class="flex my-auto mt-[10rem] p-10">
+            <div class="flex my-auto p-10">
                 <h1 class="text-white font-bold text-[2rem] md:text-[4.5rem] text-shadow">Fasilitas Konseling Sekolah Vokasi UGM</h1>
             </div> 
             <div class=" lg:w-[50vw] md:w-[90vw]  flex justify-center items-center">  
-            <div class="md:w-[70%] md:h-[800px] px-10 md:px-16 py-16 rounded-3xl bg-white shadow">
+            <div class="flex flex-col item-center md:w-[70%] md:h-fit px-10 md:px-16 py-16 rounded-3xl bg-white shadow">
               <h2 class="text-[#155458] text-center text-[2.5rem] font-bold">Login</h2>
               <p class="text-xs my-5 text-center">Login untuk mulai melakukan pendaftaran konseling</p>
-              <form class="mt-16 space-y-4">
+              <a href="{{ route('auth.google') }}" class="w-full py-5 px-4 text-center font-bold  text-sm tracking-wide rounded-2xl text-white bg-[#155458] hover:bg-[#155458da] focus:outline-none">login with google</a>
+              <a href="/" class="flex items-center gap-4 my-10">
+                <img src="{{ asset('images/back.png') }}" alt="Back" class="w-7">
+                <span class="text-[1.1rem] text-[#155458] font-bold">Back to home</span>
+              </a>
+              {{-- <form class="mt-16 space-y-4">
                 <div>
                   <div class="relative flex items-center">
                     <input name="username" type="email" required class="w-full text-[#4F4F4F] text-sm border-[1px] border-[#155458] px-7 py-4 rounded-2xl outline-[#155458] placeholder:font-semibold placeholder:text-[#4F4F4F]" placeholder="Email" />
@@ -46,14 +51,14 @@
                   </div> --}}
                 </div>
   
-                <div class="!mt-16">
+                {{-- <div class="!mt-16">
                   <button type="button" class="w-full py-5 px-4 font-bold  text-sm tracking-wide rounded-2xl text-white bg-[#155458] hover:bg-[#155458da] focus:outline-none">
                     Login
                   </button>
-                </div>
-                <p class="text-gray-800 text-sm !mt-8 text-center">Don't have an account? <a href="/register" class="text-[#155458] hover:underline ml-1 whitespace-nowrap font-semibold">Register here</a></p>
-                <a href="{{ route('auth.google') }}">login with google</a>
-              </form>
+                </div> --}}
+                {{-- <p class="text-gray-800 text-sm !mt-8 text-center">Don't have an account? <a href="/register" class="text-[#155458] hover:underline ml-1 whitespace-nowrap font-semibold">Register here</a></p> --}}
+              {{-- </form> --}}
+
             </div>
             </div>
         </div>
