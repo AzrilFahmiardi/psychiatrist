45832e43d54753bbb2394af37c88383a51f575b7
azrilfahmiardi <anakijummira@gmail.com>
2025-02-20 13:00:20 +0700

    semua agenda booking sudah sesuai user baik pasien maupun dokter

--- app/Http/Controllers/RiwayatController.php
+++ app/Http/Controllers/RiwayatController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RiwayatController extends Controller
 {
     function getBooking()
-{
-    $bookings = Booking::with(['psikolog', 'jadwal', 'konsultasi'])->get();
+    {
+        $bookings = Booking::with(['psikolog', 'jadwal', 'konsultasi'])
+            ->where('pasien_id', Auth::id())
+            ->get();
 
-    return view('riwayat', compact('bookings'));
-}
-    
+        return view('riwayat', compact('bookings'));
+    }
 }
